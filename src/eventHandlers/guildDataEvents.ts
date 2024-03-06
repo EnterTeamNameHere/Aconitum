ca64c3e5d249a3f5d8d56bce638f568d2dbe36b1
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2024-03-06 23:00:39 +0900

    Delete event

--- src/eventHandlers/guildDataEvents.ts
+++ src/eventHandlers/guildDataEvents.ts
@@ -1,11 +1,15 @@
 import {Events, Guild} from "discord.js";
 
 import Procs from "../interfaces/clientProcs.js";
-import {insert} from "../utils/guildData.js";
+import {insert, remove} from "../utils/guildData.js";
 
 const procs: Procs = function execute(client): void {
     client.on<Events.GuildCreate>(Events.GuildCreate, async (guild: Guild) => {
         await insert(guild.id);
+    });
+
+    client.on<Events.GuildDelete>(Events.GuildDelete, async (guild: Guild) => {
+        await remove(guild.id);
     });
 };
 
