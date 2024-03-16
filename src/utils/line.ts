4ef3caf58b0064e4d1d868389d24c246f4a9887e
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-03-17 08:53:29 +0900

    LINE -> Discord done

--- src/utils/line.ts
+++ src/utils/line.ts
@@ -2,9 +2,10 @@ import {EventEmitter} from "node:events";
 
 import lineSdk from "@line/bot-sdk";
 import discordSdk from "discord.js";
-import { match, replace, test, trim } from "ramda";
-// Ramda.js document https://ramdajs.com/docs/
-// import {LineMessage} from "../classes/lineMessage.js";
+import {isEmpty, isNotNil, match, replace, test, trim} from "ramda";
+
+import {LineConnection} from "../classes/lineConnection.js";
+import {LineMessage} from "../classes/lineMessage.js";
 
 export const lineEventEmitter = new EventEmitter({captureRejections: true});
 
@@ -16,15 +17,34 @@ export default {
         event: lineSdk.WebhookEvent,
     ): Promise<void> => {
         try {
-            if (event.type === "message" && event.message.type === "text" && event.source.type === "group") {
+            if (
+                event.type === "message" &&
+                event.message.type === "text" &&
+                event.source.type === "group" &&
+                isNotNil(event.source.userId)
+            ) {
+                const groupSummary = await lineClient.getGroupSummary(event.source.groupId);
+                const userProfile = await lineClient.getProfile(event.source.userId);
+
                 const commandLikeMessage = replace(/ +/g, " ", trim(event.message.text));
                 if (test(/aconitum.auth [0-9]{6}/, commandLikeMessage)) {
-                    lineEventEmitter.emit("auth", match(/[0-9]{6}/, commandLikeMessage)[0], event.source.groupId);
+                    lineEventEmitter.emit("auth", match(/[0-9]{6}/, commandLikeMessage)[0], groupSummary.groupId);
                 }
 
-                // help me
-                // const lineMessage = new LineMessage(discordClient);
-                // lineMessage.sendDiscord();
+                const connections = await LineConnection.findActive({"data.groupId": groupSummary.groupId});
+                if (isEmpty(connections)) {
+                    return;
+                }
+                for (const connection of connections) {
+                    const lineMessage = new LineMessage(discordClient)
+                        .setConnection(connection)
+                        .setGuildName(groupSummary.groupName)
+                        .setChannelName("(group)")
+                        .setUsername(userProfile.displayName)
+                        .setIcon(userProfile.pictureUrl)
+                        .setContent(event.message.text);
+                    lineMessage.sendAll();
+                }
             }
         } catch (err) {
             console.error(`[ERR]: ${err}`);
